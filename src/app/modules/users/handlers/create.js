9ac28382f2cf33063f5353b86c64117f229a63f7
jacksonAquino <jackson.primo12@gmail.com>
2021-03-04 21:28:44 -0300

    adjsted handle create user

--- src/app/modules/users/handlers/create.js
+++ src/app/modules/users/handlers/create.js
@@ -1,35 +1,23 @@
 const firestore = require('../../../firebase/firestore')
-const validateFields = (body) => {
-  const { /* campos */} = body;
-  const errors = [];
-  return errors;
-};
+const auth = require('../../../firebase/auth')
 
 module.exports = async (req, resp) => {
-  const { email, password } = req.body;
-  const errors = validateFields(req.body);
+  const body = req.body
+  const { email, password } = body;
 
-  if (errors.length) {
-    return resp
-      .status(400)
-      .json(
-        FUNCTIONS.objectReturn('Data is invalid', errors, false, 400),
-      );
-  }
-
-  try {
-		firestore.create()
-  } catch (error) {
-    console.log(error);
-    return resp
-      .status(400)
-      .json(
-        FUNCTIONS.objectReturn(
-          'Internal server error',
-          error.message,
-          false,
-          400,
-        ),
-      );
-  }
+  auth.create(email, password)
+    .then((userRecord) => {
+      body.id = userRecord.uid
+      firestore.create(body).then(res=>{
+        return resp.status(200).json({"message":"success"})
+      })
+      .catch((error)=>{
+        console.log('Error creating new user:', error.code);
+        return resp.status(400).json({"error": error.code});  
+      })
+    })
+    .catch((error) => {
+      console.log('Error creating new user:', error.code);
+      return resp.status(400).json({"error": error.code});
+    });
 };
