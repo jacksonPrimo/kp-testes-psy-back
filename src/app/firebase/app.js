17040fb724978d1e28a3756a32b04f3800b0236b
jacksonAquino <jackson.primo12@gmail.com>
2021-03-08 12:33:40 -0300

    adjustment

--- src/app/firebase/app.js
+++ src/app/firebase/app.js
@@ -1,18 +1,18 @@
 const admin = require('firebase-admin');
 
 // const serviceAccount = require('./secretKey.json');
-const secretKey = require('./secretKey.json');
+// const secretKey = require('./secretKey.json');
 const serviceAccount = {
-  "type": process.env.type || secretKey.type,
-  "project_id": process.env.project_id || secretKey.project_id,
-  "private_key_id": process.env.private_key_id || secretKey.private_key,
-  "private_key": process.env.private_key || secretKey.private_key,
-  "client_email": process.env.client_email || secretKey.client_email,
-  "client_id": process.env.client_id || secretKey.client_id,
-  "auth_uri": process.env.auth_uri || secretKey.auth_uri,
-  "token_uri": process.env.token_uri || secretKey.token_uri,
-  "auth_provider_x509_cert_url": process.env.auth_provider_x509_cert_url || secretKey.auth_provider_x509_cert_url,
-  "client_x509_cert_url": process.env.client_x509_cert_url || secretKey.client_x509_cert_url,
+  "type": process.env.type,
+  "project_id": process.env.project_id,
+  "private_key_id": process.env.private_key_id,
+  "private_key": process.env.private_key,
+  "client_email": process.env.client_email,
+  "client_id": process.env.client_id,
+  "auth_uri": process.env.auth_uri,
+  "token_uri": process.env.token_uri,
+  "auth_provider_x509_cert_url": process.env.auth_provider_x509_cert_url,
+  "client_x509_cert_url": process.env.client_x509_cert_url,
 };
 
 admin.initializeApp({
